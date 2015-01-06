534b96e0c20133fc9e16ead603625634cb2a6606
GTxx <xiongxiong1986@gmail.com>
2015-01-06 23:23:01 +0800

    Rendering Less Templates

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var connect = require('connect')
-  , makeJade = require('./lib/processor/jade.js');
+  , makeJade = require('./lib/processor/jade.js')
+  , makeLess = require('./lib/processor/less.js');
 
 var createMiniHarp = function(path){
   var app = connect();
+  app.use(makeLess(path));
   app.use(makeJade(path));
   return app;
 
