33263c08eb88fccb28d5303bf1fc1e94406052b0
GTxx <xiongxiong1986@gmail.com>
2015-01-07 00:32:09 +0800

    setting content type

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var connect = require('connect')
+  , path = require('path')
   , makeJade = require('./lib/processor/jade.js')
   , makeLess = require('./lib/processor/less.js');
 
-var createMiniHarp = function(path){
+var createMiniHarp = function(dir_path){
   var app = connect();
+  app.use(function(req, res, next){
+    var ext = path.extname(req.url);
+    if (ext == '.less' || ext == '.jade'){
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not Found')
+    }
+    next();
+  })
   app.use(function(req, res, next){
     if (req.url == '/'){
       req.url = "/index.html";
     }
     next();
   });
-  app.use(makeJade(path));
-  app.use(makeLess(path));
+  app.use(makeLess(dir_path));
+  app.use(makeJade(dir_path));
+
   return app;
 
 }
